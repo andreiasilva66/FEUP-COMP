c7149d93a42477d7f1fa1244443c42288acbdc53
andreiasilva <up202108769@g.uporto.pt>
2024-02-25 17:22:30 +0000

    Precedencias corrigidas

--- src/main/pt/up/fe/comp2024/symboltable/JmmSymbolTable.java
+++ src/main/pt/up/fe/comp2024/symboltable/JmmSymbolTable.java
@@ -67,6 +67,7 @@ public class JmmSymbolTable implements SymbolTable {
 
     @Override
     public Type getReturnType(String methodSignature) {
+
         // TODO: Simple implementation that needs to be expanded
         return new Type(TypeUtils.getIntTypeName(), false);
     }
